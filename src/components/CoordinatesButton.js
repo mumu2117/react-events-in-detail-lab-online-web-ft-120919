d71561c350d2c4bde1a24c12957ec4c5548c9f70
mumu2117 <jmillerarcher@me.comgit config --global user.name mumu2117>
2020-05-02 18:41:29 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+    handleClick = (e) => {
+        let x = e.clientX
+        let y = e.clientY
+        let coords = [x, y]
+        this.props.onReceiveCoordinates(coords)
+    }
+
+    render() {
+        return (
+            <div className='coordinates-button'>
+                <button onClick={this.handleClick}>Get Mouse Coordinates</button>
+            </div>
+        );
+    }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+    handleClick = (event) => {
+        const delayedFunction = this.props.onDelayedClick 
+        event.persist()
+        setTimeout(function () { delayedFunction(event) }, this.props.delay)
+    }
+
+
+    render() {
+        return (
+            <div className='delayed-button'>
+                <button onClick = {this.handleClick.bind(this)}>
+                    Waiting...
+                </button>
+            </div>
+        );
+    }
+}
+
+export default DelayedButton;
